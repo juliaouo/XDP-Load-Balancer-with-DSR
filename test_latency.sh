0d3ca536bb02f2b7bf84327b9ec0345917d39b41
engine <tlc0720512@gmail.com>
2025-05-27 15:48:22 +0800

    modify test case

--- test_latency.sh
+++ test_latency.sh
@@ -4,6 +4,20 @@
 clients=(client-test1 client-test2 client-test3)
 target_ip=10.10.0.5
 
+# for c in "${clients[@]}"; do
+#   docker exec $c sh -c "
+#     for i in \$(seq 1 1000); do
+#       curl -o /dev/null -s -w '%{time_total}\n' http://$target_ip:80/cpu &
+#       if (( i % 100 == 0 )); then
+#         wait
+#       fi
+#     done
+#     wait
+#   " > ${c}_latency_cpu.txt &
+# done
+
+# wait
+
 for c in "${clients[@]}"; do
   docker exec $c sh -c "for i in \$(seq 1 1000); do curl -o /dev/null -s -w '%{time_total}\n' http://$target_ip:80/cpu; done" > ${c}_latency_cpu.txt &
 done
@@ -18,4 +32,4 @@ done
 
 wait
 
-cat client*_latency_io.txt | awk '{sum+=$1} END {print "平均 io latency:", sum/NR, "秒"}'
+cat client*_latency_io.txt | awk '{sum+=$1} END {print "平均 io latency:", sum/NR, "秒"}'s
